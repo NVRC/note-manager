228952516c6387376366a3ee69f8e2185083639e
NVRC <nathaniel.charlebois@gmail.com>
2018-11-21 18:18:30 -0500

    Implemented list note display and drawer navigation

--- components/note-display.js
+++ components/note-display.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import {    Container,
             Content,
             Text,
+            List,
+            ListItem
                 } from "native-base";
 
 //          SQLite supported on Expo
@@ -13,14 +15,56 @@ const DB_STR = 'notes.db';
 const db = SQLite.openDatabase(DB_STR);
 
 
+
 export default class NoteDisplay extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {  items: null,
+                        };
+    }
+
+    componentDidMount(){
+        db.transaction(tx => {
+            tx.executeSql(
+                `select * from notes;`,
+                [],
+                (_, { rows: { _array } }) => this.buildStateArray( _array )
+            );
+        });
+    }
+
+    buildStateArray( src ){
+        let array = [];
+        for(let i = 0; i < src.length; i++){
+            array.push(src[i]);
+        }
+        this.setState({items: array});
+        return array;
+    }
+
+
+
     render() {
+        var items =[];
+        if(this.state.items != null){
+            items = this.state.items.map(element => {
+                return element.title;
+            });
+        }
+
         return (
             <Container>
                 <Content>
                     <HeaderComponent {...this.props}/>
                     <Text>Note Display</Text>
+                    <List dataArray={items}
+                        renderRow={(item) =>
+                        <ListItem>
+                        <Text>{item}</Text>
+                        </ListItem>
+                        }>
+                    </List>
                 </Content>
             </Container>
         );
